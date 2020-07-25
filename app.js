9be97275dcdc11c500657c10340455d45a0e637c
Aicha <aicha@louraghi.com>
2020-07-25 17:19:35 +0200

    Few changes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
@@ -15,7 +16,7 @@ app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 //Routes
 app.use("/", require("./routes/index"));
-
+app.use(express.static(path.join(__dirname, "public")));
 const port = process.env.PORT || 5000;
 app.listen(
   port,
